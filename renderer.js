b2de4aee40e63299a740c88ad04f7ac907b4ef4e
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2024-03-10 15:22:25 +0100

    Added renderer the ability to play a sound effect

--- renderer.js
+++ renderer.js
@@ -2,10 +2,13 @@ export class Renderer {
     #text;
     #bg;
     #bgm = new Audio();
+    #sfx = new Audio();
 
     constructor () {
         this.#text = document.getElementById("text");
         this.#bg = document.getElementById("image");
+        this.#bgm.loop = true;
+        this.#sfx.loop = false;
     }
 
     render(frame) {
@@ -25,6 +28,9 @@ export class Renderer {
             this.#bgm.src = "bgm/" + frame.bgm;
             this.#bgm.play();
         }
+
+        this.#sfx.src = "sfx/" + frame.sfx;
+        this.#sfx.play();
     }
     #typeWriter(txt, speed) {
         let i = 0;
